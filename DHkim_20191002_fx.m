94e67c76239ceee3e7497001d1932974d18c8c12
Gajeju <45118610+Gajeju@users.noreply.github.com>
2019-11-13 19:41:21 +0900

    matlab 기초

---
+++ DHkim_20191002_fx.m
@@ -0,0 +1,7 @@
+x = 9;
+a = 1;
+b = 3;
+c = 5;
+poly = a * x^2 + b * x + c;
+denom = 4 * pi* x^2 + cos(x-2) * poly;
+f = (log(poly) - sin(poly))/denom
---
+++ DHkim_20191023_V.m
@@ -0,0 +1,13 @@
+h = [1 5 12];
+r = 3;
+
+cylinV = pi * r^2 * h
+
+b = [2 4 6];
+triHigh = 12;
+
+triLarge = triHigh * 0.5 * b
+
+triZ = 6;
+
+triV = triLarge * triZ
